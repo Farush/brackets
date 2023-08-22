04c23bcbb3a7f68b14e5ec5f0597ad9d33c7c1be
Farkhad Sibgatullin <farusha.q@gmail.com>
2023-08-22 21:55:31 +0300

    fix: problem solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let idx = 0;
+  let openingBracketsArray = bracketsConfig.map(bracket => bracket[0]);
+  let closingBracketsArray = bracketsConfig.map(bracket => bracket[1]);
+  let arr = str.split('');
+
+
+  const isAdjacent = (idx) => {
+    if (openingBracketsArray.indexOf(arr[idx]) === -1) return false
+    if (openingBracketsArray.indexOf(arr[idx]) === closingBracketsArray.indexOf(arr[idx + 1])) return true;
+    else return false
+  }
+
+  const hasAdjacent = () => {
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (isAdjacent(i)) return true;
+    }
+    return false;
+  }
+
+  if ((arr.length % 2 !== 0) || (!openingBracketsArray.includes(arr[0]))) return false
+
+  else {
+    while (hasAdjacent()) {
+      idx = 0
+      while (true) {
+        if (isAdjacent(idx)) {
+          arr.splice(idx, 2);
+          break
+        }
+        idx++;
+      }
+
+    }
+    if (arr.length === 0) return true;
+  }
+  return false
 }
